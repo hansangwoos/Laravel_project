e73b4708dcb1bde28c09b201d565380ae1e1ea14
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-02 11:48:31 +0900

    feat : 로그인, 회원가입, 대시보드 라우트 구성

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+// Route::get('/', function () {
+//     return view('auth.login');
+// });
 
+// 로그인 관련
+Route::get("/login",[AuthController::class , 'showlogin'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/logout',[AuthController::class,'logout']);
+
+// 회원가입 관련
+Route::get('/register',[AuthController::class,'register'])->name('register');
+Route::post('/register',[AuthController::class,'createUser']);
+
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
 
