01668b7c0c7261d4a5eb115057848c9a7f7e3d2b
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-03 13:06:02 +0900

    feat : new Route configration new create middleware group - That group checks auth and lunchCheck - another group is check only auth - and another one not check just route

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,45 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
-use Illuminate\Container\Attributes\Auth;
+use App\Http\Controllers\LunchBreakController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('auth.login');
-// });
+/**
+ * new Route 구성 2025-09-03
+ */
+
+// 미들웨어 그룹으로 묶어서 사용하기
+// 인증이 필요하고 점심시간을 체크해야하는 페이지들끼리 middleware group 화
+Route::middleware(['auth','LunchCheck'])->group(function(){
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
+});
+
+// 인증만 필요한 페이지
+Route::middleware('auth')->group(function(){
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/logout',[AuthController::class,'logout']);
+});
+
+// 인증이 필요없는 페이지 구성
+
+Route::get('/', function () {
+
+    if(Auth::check()){
+        return view('dashboard.index');
+    }else {
+        return view('auth.login');
+    }
+
+});
 
 // 로그인 관련
 Route::get("/login",[AuthController::class , 'showlogin'])->name('login');
 Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout']);
 
 // 회원가입 관련
 Route::get('/register',[AuthController::class,'register'])->name('register');
 Route::post('/register',[AuthController::class,'createUser']);
 
-Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
-
+// 그 외 페이지 관련
+Route::get('/lunchBreak',[LunchBreakController::class,'index'])->name('lunchBreak');
