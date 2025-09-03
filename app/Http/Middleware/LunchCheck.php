d08c1b449c0b6aafc3e862f5ea81819d2596cd62
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-03 12:55:52 +0900

    feat : LunchCheck meddleware feat this middlewhare 12:00 ~ 13:00 break time set cannot be used outside the login page

---
+++ app/Http/Middleware/LunchCheck.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LunchCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $lunchBreakFirsttime = '12:00';
+        $lunchBreakLasttime = '13:00';
+
+        $totime = date("H:i");
+
+        if($totime >= $lunchBreakFirsttime && $totime <= $lunchBreakLasttime){
+            return redirect(route('lunchBreak'));
+        }
+        return $next($request);
+    }
+}
