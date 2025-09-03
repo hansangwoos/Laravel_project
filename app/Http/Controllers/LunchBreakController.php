f807ffcee15471d093b9a5df29066b1cbe6f43db
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-03 12:57:37 +0900

    feat : LunchBreakController feat index() method add this controller show views/errors/lunchBreak

---
+++ app/Http/Controllers/LunchBreakController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LunchBreakController extends Controller
+{
+    public function index(){
+        return view('errors.lunchBreak');
+    }
+}
