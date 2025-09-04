58480f8201dc0e1e3164fb0e0748a3f91259ba3c
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-04 14:24:03 +0900

    feat : add js,css plugins laravel input value and add server config localhost

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,24 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/dashboard.css',
+                'resources/js/dashboard.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
+        },
+        watch: {
+            usePolling: true,  // 파일 감시 방법 변경
+        }
+    },
 });
