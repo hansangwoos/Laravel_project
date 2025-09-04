78aa093c799a36025accef1b427b3dec24e96fae
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-04 14:04:33 +0900

    feat : use new controller ProductsController chage the Route::middleware action and  changed Route::get('/') return view dashboard a to redirect(route(dashboard)

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LunchBreakController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
+
 
 /**
  * new Route 구성 2025-09-03
@@ -13,13 +16,14 @@ use Illuminate\Support\Facades\Route;
 // 미들웨어 그룹으로 묶어서 사용하기
 // 인증이 필요하고 점심시간을 체크해야하는 페이지들끼리 middleware group 화
 Route::middleware(['auth','LunchCheck'])->group(function(){
-    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard')->middleware('auth');
+    Route::get('/dashboard',[ProductsController::class,'index'])->name('dashboard')->middleware('auth');
 });
 
 // 인증만 필요한 페이지
 Route::middleware('auth')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/logout',[AuthController::class,'logout']);
+
 });
 
 // 인증이 필요없는 페이지 구성
@@ -27,7 +31,7 @@ Route::middleware('auth')->group(function(){
 Route::get('/', function () {
 
     if(Auth::check()){
-        return view('dashboard.index');
+        return redirect(route('dashboard'));
     }else {
         return view('auth.login');
     }
