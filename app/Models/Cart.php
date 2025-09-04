41557928aa47ee74bcff8ae2531c3748d0cf2e9a
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-04 14:12:09 +0900

    feat : add new Cart Model and Database(mysql) - product relataionship model product belongTo() user belongTo()

---
+++ app/Models/Cart.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+     protected $fillable = [
+        'product_id',
+        'user_id',
+    ];
+
+    // 카트 -> 상품
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    // 카트 -> 유저
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
