167c1ad35b0c41a1a2c85e62f0f528b2e98fdc42
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-04 14:05:07 +0900

    feat : add Cart function Controller

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+
+
+class CartController extends Controller
+{
+
+    public function store(Request $request){
+
+    }
+}
