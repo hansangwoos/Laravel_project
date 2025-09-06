5b7066193ad0098034d0c53049283e598fd931f8
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-06 14:46:54 +0900

    feat : RESTful 기반 Route에 맞춘 CartController 수정 및 장바구니 기능 추가, PSR표준에 따른 이름 수정
    - addToCart : 상품목록에서 [담기] 버튼의 값을 받아 cart 에 저장
    - clearCart : 장바구니 내 전체 삭제

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,7 +10,53 @@ use Illuminate\Support\Facades\Auth;
 class CartController extends Controller
 {
 
-    public function store(Request $request){
+    public function addToCart(Request $request){
+        $productId = $request->input('productId');
+        $userId = Auth::id();
 
+        $existingCart = Cart::where('user_id', $userId)
+                            ->where('product_id', $productId)
+                            ->first();
+
+        if($existingCart){
+            $existingCart->quantity += 1;
+            $existingCart->save();
+        }else {
+            Cart::create([
+                'product_id' => $productId,
+                'user_id' => $userId,
+                'quantity' => 1
+            ]);
+        }
+
+        // 업데이트 된 장바구니 정보
+        $cartCount = Cart::where('user_id', $userId)->sum('quantity');
+        $carts = Cart::with('product')
+                ->where('user_id', $userId)
+                ->get();
+
+        // 랜더링
+        $cartHtml = view('dashboard.partials.cart-section', compact('carts','cartCount'))->render();
+        return response()->json([
+            'success'=> true,
+            'cartHtml'=>$cartHtml,
+        ]);
     }
+
+    // 전체삭제
+    public function clearCart(){
+        Cart::where('user_id',Auth::id())->delete();
+
+        // 삭제 후 빈 장바구니 상태로 다시 조회
+        $cartCount = 0; // 또는 Cart::where('user_id', $userId)->sum('quantity');
+        $carts = collect(); // 빈 컬렉션, 또는 Cart::with('product')->where('user_id', $userId)->get();
+
+        // 랜더링
+        $cartHtml = view('dashboard.partials.cart-section', compact('carts','cartCount'))->render();
+        return response()->json([
+            'success'=>true,
+            'cartHtml'=>$cartHtml,
+        ]);
+    }
+
 }
