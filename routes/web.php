fd2fdaf698e9bbb810a40be79fc5bc2ec6fad1d4
hansangwoo(laptop) <hansangwoos456@gmail.com>
2025-09-06 15:15:10 +0900

    feat : 장바구니 관련 Route 추가 장바구니 추가 전체삭제 업데이트 관련 /cart route에  post,delete,put 추가

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LunchBreakController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
@@ -17,6 +18,19 @@ use Illuminate\Http\Response;
 // 인증이 필요하고 점심시간을 체크해야하는 페이지들끼리 middleware group 화
 Route::middleware(['auth','LunchCheck'])->group(function(){
     Route::get('/dashboard',[ProductsController::class,'index'])->name('dashboard')->middleware('auth');
+
+    /**
+     * 장바구니 관련
+     */
+    // 장바구니 추가
+    Route::post('/cart/add', [CartController::class,'addToCart']);
+    // 장바구니 전체삭제
+    Route::delete('/cart/clear',[CartController::class,'clearCart']);
+    // 장바구나 - or + 의 업데이트
+    Route::put('/cart/{cartId}',[CartController::class,'updateCartItem']);
+    // 장바구니 상품 1개의 - 시 상품 삭제
+    Route::delete('/cart/{cartId}',[CartController::class,'removeCartItem']);
+
 });
 
 // 인증만 필요한 페이지
